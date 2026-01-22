70e380b874e6172310974bc8845ceb8bbfdd3bc1
KamelABA <123214040+KamelABA@users.noreply.github.com>
2026-01-22 12:55:54 +0100

    Update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -4,9 +4,6 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from sqlalchemy.orm import Session
-from database import get_db
-import models
 import schemas
 import os
 from dotenv import load_dotenv
